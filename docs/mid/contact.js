b8382c3dbaa0457dad10c29092b551ff231c7924
joshhy99 <koltongkoltong3@gmail.com>
2021-03-15 18:35:56 -0700

    final polish

---
+++ docs/mid/contact.js
@@ -0,0 +1,23 @@
+function validateForm(){
+    let validName = false, validEmail = false, validComment = false;
+    let errorMessages = "";
+
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
+
+    if (name === null || name === "" || name.length > 50 || !name.match(letters))
+        errorMessages += "<p>Please enter a valid name (50 characters or fewer).</p>";
+    else validName = true;
+
+    if(!/\S+@\S+\.\S+/.test(email))
+        errorMessages += "<p>Email is required and must be a valid address.</p>";
+    else validEmail = true;
+
+    if(message.length < 10 || message.length > 2000)
+        errorMessages += "<p>Message required (at least 10 characters and fewer than 2000 characters).</p>"
+
+    document.getElementById("errorMessages").innerHTML = errorMessages;
+
+    return (validName && validEmail && validComment);
+}
